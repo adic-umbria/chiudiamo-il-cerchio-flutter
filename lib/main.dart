6101e54e608d6cda6810ceffd1283249d72c7798
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2020-04-21 22:44:40 +0200

    Import `TheAlphamerc/flutter_smart_course` theme
    Theme imported from: https://github.com/TheAlphamerc/flutter_smart_course

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
+import 'package:chiudiamo_il_cerchio/theme/theme.dart';
 import 'package:flutter/material.dart';
 
-import './screens/home.dart';
+import './screens/home_page.dart';
 
-void main() => runApp(HomeScreen());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}

b6255a7c58540cd7d5195d58fa7f00fde747305c
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2020-04-21 21:59:29 +0200

    Add `/pages/` responses to fixtures list

--- test/fixtures.dart
+++ test/fixtures.dart
@@ -28,6 +28,59 @@ class Fixtures {
       "title": "Chi siamo"
     }    
   ''');
+  static final String pagesResponse = '''
+    {
+      "output": {
+        "data": {
+          "count": 2,
+          "items": [
+            {
+              "created": "2020-04-21T12:00:00+02:00",
+              "description": "Something to show",
+              "draft": false,
+              "outputs": [
+                {
+                  "type": "html",
+                  "url": "https://example.com/blog/chi-siamo/"
+                }
+              ],
+              "permalink": "https://example.com/blog/chi-siamo/",
+              "relpermalink": "/blog/chi-siamo/",
+              "title": "Chi siamo",
+              "type": "page",
+              "updated": "2020-04-21T12:00:00+02:00",
+              "weight": 0
+            },
+            {
+              "created": "2020-04-21T12:00:00+02:00",
+              "description": "Something to show",
+              "draft": false,
+              "outputs": [
+                {
+                  "type": "html",
+                  "url": "https://example.com/micro/il-progetto/"
+                }
+              ],
+              "permalink": "https://example.com/micro/il-progetto/",
+              "relpermalink": "/micro/il-progetto/",
+              "title": "Il Progetto",
+              "type": "page",
+              "updated": "2020-04-21T12:00:00+02:00",
+              "weight": 0
+            }
+          ],
+          "taxonomy": "tag",
+          "term": "in-app"
+        }
+      }
+    }
+  ''';
+  static final String pagesResponseError = '''
+    {
+      "status": "error",
+      "code": 500
+    }
+  ''';
   static final Page page = Page(
     created: '2020-04-21T12:00:00+02:00',
     description: 'Something to show',

8ede25a0d62b81d638bccc6dc6e3f3a6610bd97a
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2020-04-21 17:11:32 +0200

    Refactor to an empty app

---
+++ lib/screens/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
